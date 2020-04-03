10fe38b0210548c992684ce67c0903904f018ddc
Sarah Schwartz <seschwartz8@gmail.com>
2020-04-03 08:41:02 -0700

    build restaurant model

--- RestaurantAPI/Models/Restaurant.cs
+++ RestaurantAPI/Models/Restaurant.cs
@@ -6,6 +6,19 @@ namespace RestaurantAPI.Models
 {
   public class Restaurant
   {
+    public int RestaurantId { get; set; }
 
+    [StringLength(100)]
+    [Required]
+    public string Name { get; set; }
+
+    [Required]
+    public string Address { get; set; }
+
+    [Required]
+    public string Description { get; set; }
+
+    [Required]
+    public int Rating { get; set; }
   }
 }
